db010cacf7a6c77ce781931f491e22e25e4f2dc5
samko5sam <75727536+samko5sam@users.noreply.github.com>
2021-09-25 16:01:35 +0800

    update new version of alsoweather

--- app.js
+++ app.js
@@ -1,148 +1,180 @@
-let CWB_KEY = "CWB-1485B01A-7416-4E72-8ABB-E351980FD554";
-let CWB_API = `https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=${CWB_KEY}&format=JSON`;
-let QUICK_CHART_API = `https://quickchart.io/chart?bkg=white&c=`
+// declare keys
 
+// api key
+const CWB_KEY = "CWB-1485B01A-7416-4E72-8ABB-E351980FD554";
+// api url
+const CWB_API = `https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=${CWB_KEY}&format=JSON`;
+// api url
+const QUOTE_API = `https://api.quotable.io/random`
+
+// declare objects
+const fetched_quote_data = {
+  content: null,
+  author: null
+}
+let fetched_cwb_data = {}
+let parsed_cwb_data = {
+  now: {}
+}
+window.model = {
+  mylocation: ''
+}
+
+// grab html dom element
 let cwb_location_select = document.getElementById("cwb_location_select")
-let maxt_chart_image = document.getElementById("maxt_chart_image")
-let maxt_chart_element
-let mint_chart_image = document.getElementById("mint_chart_image")
-let mint_chart_element
+let quote_block_inner_content = document.getElementById("quote_block_inner_content")
+let loading_quote_skeleton = document.getElementById("loading_quote_skeleton")
+let weather_block_inner_content = document.getElementById("weather_block_inner_content")
+let loading_weather_skeleton = document.getElementById("loading_weather_skeleton")
 
-cwb_location_select.addEventListener("input", locationInputService)
+// eventlistener
+cwb_location_select.addEventListener("input",locationInputService)
 
-fetchcwb(CWB_API)
+// init
+document.addEventListener("DOMContentLoaded", () => {
+  restorelocation()
+  fetchcwb(CWB_API)
+  console.log(CWB_API)
+  fetchquote(QUOTE_API)
+  console.log(QUOTE_API)
+})
 
+// fetch cwb api
 async function fetchcwb(url) {
-  let responseData;
   try {
-    await fetch(url,{
+    const response = await fetch(url,{
       method : "GET"
-    }).then((response) => {
-      return response.json();
-    }).then(response => {
-      responseData = response
     })
+    const jsonData = await response.json();
+    console.log(jsonData.records.datasetDescription)
+    fetched_cwb_data = await jsonData.records.location
+    let location_index = find_location_index(fetched_cwb_data,cwb_location_select.value)
+    console.log(fetched_cwb_data[location_index].weatherElement[0].time[0].parameter.parameterName)
+    parsed_cwb_data.now.Wx = fetched_cwb_data[location_index].weatherElement[0].time[0].parameter.parameterName
+    parsed_cwb_data.now.PoP = fetched_cwb_data[location_index].weatherElement[1].time[0].parameter.parameterName
+    parsed_cwb_data.now.MinT = fetched_cwb_data[location_index].weatherElement[2].time[0].parameter.parameterName
+    parsed_cwb_data.now.CI = fetched_cwb_data[location_index].weatherElement[3].time[0].parameter.parameterName
+    parsed_cwb_data.now.MaxT = fetched_cwb_data[location_index].weatherElement[4].time[0].parameter.parameterName
+
+    fillin_weather(generate_now_weather(parsed_cwb_data.now),weather_block_inner_content,loading_weather_skeleton)
+
   } catch (error) {
     alert(error.message)
   }
+}
+
+async function fetchquote(url) {
   try {
-    let locationNameIndex;
-    restorelocation()
-    cwb_location_select = document.getElementById("cwb_location_select")
-    for (let i = 0; i < responseData.records.location.length; i++) {
-      let locationName = await responseData.records.location[i].locationName
-      if (cwb_location_select.value == locationName){
-        locationNameIndex = i
-      }
-    }
-    console.log("location index: ",locationNameIndex)
-    let weatherElement = await responseData.records.location[locationNameIndex].weatherElement
-    console.log(weatherElement)
-    let maxt_chart_image_data = {
-      type: "line",
-      data: {}
-    }
-    let mint_chart_image_data = {
-      type: "line",
-      data: {}
-    }
-    for (let i = 0; i < weatherElement.length; i++) {
-      let weatherElementName = await weatherElement[i].elementName
-      // 如果是最大溫度
-      if (weatherElementName == "MaxT"){
-        let data = []
-        for (let z = 0; z < weatherElement[i].time.length; z++) {
-          const element = weatherElement[i].time[z];
-          
-          data.push(time(element.startTime))
-          if (maxt_chart_image_data.data.labels == undefined) maxt_chart_image_data.data.labels = []
-          // maxt_chart_image_data.data.labels.push(element.startTime)
-          // 如果是最後一個Time
-          if (z == weatherElement[i].time.length-1){
-            data.push(time(element.endTime))
-          }
-        }
-        // 產生標籤
-        for (let y = 0; y < data.length-1; y++) {
-          let text = data[y]+" - "+data[y+1]
-          console.log(text)
-          maxt_chart_image_data.data.labels.push(text)
-        }
-        for (let z = 0; z < weatherElement[i].time.length; z++) {
-          const element = weatherElement[i].time[z];
-          if (maxt_chart_image_data.data.datasets == undefined) maxt_chart_image_data.data.datasets = [{
-            label: "最大溫度",
-            data: []
-          }]
-          maxt_chart_image_data.data.datasets[0].data.push(element.parameter.parameterName)
-        }
-        console.log(JSON.stringify(maxt_chart_image_data))
-        // maxt_chart_image.src = QUICK_CHART_API + JSON.stringify(maxt_chart_image_data)
-        if (maxt_chart_element != undefined) maxt_chart_element.destroy();
-        maxt_chart_element = chart(maxt_chart_image, maxt_chart_image_data)
-      }
-      // 如果是最小溫度
-      if (weatherElementName == "MinT"){
-        let data = []
-        for (let z = 0; z < weatherElement[i].time.length; z++) {
-          const element = weatherElement[i].time[z];
-          
-          data.push(time(element.startTime))
-          if (mint_chart_image_data.data.labels == undefined) mint_chart_image_data.data.labels = []
-          // 如果是最後一個Time
-          if (z == weatherElement[i].time.length-1){
-            data.push(time(element.endTime))
-          }
-        }
-        // 產生標籤
-        for (let y = 0; y < data.length-1; y++) {
-          let text = data[y]+" - "+data[y+1]
-          console.log(text)
-          mint_chart_image_data.data.labels.push(text)
-        }
-        for (let z = 0; z < weatherElement[i].time.length; z++) {
-          const element = weatherElement[i].time[z];
-          if (mint_chart_image_data.data.datasets == undefined) mint_chart_image_data.data.datasets = [{
-            label: "最小溫度",
-            data: []
-          }]
-          mint_chart_image_data.data.datasets[0].data.push(element.parameter.parameterName)
-        }
-        console.log(JSON.stringify(mint_chart_image_data))
-        if (mint_chart_element != undefined) mint_chart_element.destroy();
-        mint_chart_element = chart(mint_chart_image, mint_chart_image_data)
-      }
-    }
+    const response = await fetch(url,{
+      method : "GET"
+    })
+    const jsonData = await response.json();
+    fetched_quote_data.content = await jsonData.content
+    fetched_quote_data.author = await jsonData.author
+    console.log("quote fetch complete")
+    fillin_quote(fetched_quote_data, quote_block_inner_content, loading_quote_skeleton)
   } catch (error) {
     alert(error.message)
   }
 }
 
-
-function chart(element,params){
-  let ctx = element
-  canvas = new Chart(ctx, params);
-  return canvas
+// xdata
+function xdata() {
+  return model
 }
 
-function time(str){
-  let m = new Date(str)
-  let dateString = `${m.getUTCMonth()+1}/${m.getUTCDate()} ${m.getUTCHours()}點`;
-  return dateString
+// refresh
+function refresh() {
+  // set loading class
+  loading_weather_skeleton.classList.toggle("invisible")
+  cwb_location_select.parentElement.classList.add("is-loading")
+  console.log("refresh now")
+  fetchcwb(CWB_API)
 }
 
+// location input service
 function locationInputService(){
-  fetchcwb(CWB_API);
+  refresh()
   console.log(cwb_location_select.value)
   localStorage.setItem("locationSelected",cwb_location_select.value)
 }
+
+// fillin_quote
+function fillin_quote(data_element, element_to_fill_in, element_to_remove) {
+  element_to_fill_in.innerHTML = `<span class="is-large">${data_element.content}</span>`
+  element_to_fill_in.innerHTML += `<br><span class="has-text-grey">- ${data_element.author}</span>`
+  element_to_remove.remove()
+}
+
+// fillin_weather
+function fillin_weather(element, element_to_be_fill_in, element_to_remove) {
+  element_to_be_fill_in.innerHTML = ""
+  element_to_be_fill_in.appendChild(element)
+  element_to_remove.classList.toggle("invisible")
+  cwb_location_select.parentElement.classList.remove("is-loading")
+}
+
+// restorelocation
 function restorelocation(){
-  cwb_location_select = document.getElementById("cwb_location_select")
   let data = localStorage.getItem("locationSelected")
   if (data){
     cwb_location_select.value = data
+    model.mylocation = data
   } else {
     cwb_location_select.value = "新竹市"
+    model.mylocation = "新竹市"
   }
-  $( "#cwb_location_select" ).selectmenu( "refresh" );
+}
+
+// find location index
+function find_location_index(element,locationNametosearch) {
+  let locationNameIndex
+  cwb_location_select = document.getElementById("cwb_location_select")
+  for (let i = 0; i < element.length; i++) {
+    let locationName = element[i].locationName
+    if (locationNametosearch == locationName){
+      locationNameIndex = i
+    }
+  }
+  console.log("location index: ",locationNameIndex)
+  return locationNameIndex
+}
+
+// use now weather state object to generate html code
+function generate_now_weather(nowstate) {
+  let div = document.createElement("div")
+  div.innerHTML += `
+  <nav class="level">
+    <div class="level-item has-text-centered">
+      <div>
+        <p class="heading">降雨機率</p>
+        <p class="title"><i class="fas fa-cloud-showers-heavy"></i> ${nowstate.PoP}%</p>
+      </div>
+    </div>
+    <div class="level-item has-text-centered">
+      <div>
+        <p class="heading">最低溫度</p>
+        <p class="title"><i class="fas fa-chevron-down"></i> ${nowstate.MinT}°C</p>
+      </div>
+    </div>
+    <div class="level-item has-text-centered">
+      <div>
+        <p class="heading">最高溫度</p>
+        <p class="title"><i class="fas fa-chevron-up"></i> ${nowstate.MaxT}°C</p>
+      </div>
+    </div>
+  </nav>
+
+  <div class="tile is-ancestor">
+    <div class="tile is-12 is-parent">
+      <div class="tile is-child box">
+        <p class="title">${nowstate.Wx}</p>
+      </div>
+      <div class="tile is-child box">
+        <p class="title">${nowstate.CI}</p>
+      </div>
+    </div>
+  </div>
+  `
+  return div
 }
