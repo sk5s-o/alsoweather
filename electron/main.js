0b2c95633a7cc432ab3ae8005257103be06430ce
samko5sam <75727536+samko5sam@users.noreply.github.com>
2021-09-25 17:59:25 +0800

    add cap app and electron app

---
+++ electron/main.js
@@ -0,0 +1,88 @@
+const { app, BrowserWindow, Menu } = require('electron')
+const path = require('path')
+
+const isMac = process.platform === 'darwin'
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+	webPreferences: {
+		devTools: false
+	},
+	icon: path.join(__dirname, 'icon.png')
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+const template = [
+  // { role: 'appMenu' }
+  ...(isMac ? [{
+    label: app.name,
+    submenu: [
+      { role: 'about' },
+      { type: 'separator' },
+      { role: 'services' },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideOthers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' }
+    ]
+  }] : []),
+  // { role: 'fileMenu' }
+  {
+    label: '檔案',
+    submenu: [
+      isMac ? { role: 'close' } : { role: 'quit' }
+    ]
+  },
+  // { role: 'windowMenu' }
+  {
+    label: '視窗',
+    submenu: [
+      { role: 'minimize' },
+      ...(isMac ? [
+        { type: 'separator' },
+        { role: 'front' },
+        { type: 'separator' },
+        { role: 'window' }
+      ] : [
+        { role: 'close' }
+      ])
+    ]
+  },
+  {
+    role: "Help",
+    submenu: [
+      {
+        label: 'alsoweather website',
+        click: async () => {
+          const { shell } = require('electron')
+          await shell.openExternal('https://www.sk5s.cyou/alsoweather/')
+        }
+      }
+    ]
+  }
+]
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
